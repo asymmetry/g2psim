5662abc21862f1c159937e5ce02da1c3e81283c2
Jie Liu <jie@jlab.org>
2015-01-13 10:46:35 -0500

    Add new energy loss models (ionization, internal & external bremsstrahlung)

--- include/G2PMaterial.hh
+++ include/G2PMaterial.hh
@@ -8,7 +8,8 @@
 
 // History:
 //   Sep 2013, C. Gu, First public version.
-//   Oct 2013, J. Liu, Check formulas.
+//   Oct 2013, J. Liu, Check formulas
+//   Nov 2014, J. Liu, Add ionization fluctuation, internal and external bremsstrahlung.
 //
 
 #ifndef G2P_MATERIAL_H
@@ -19,32 +20,44 @@
 class G2PMaterial : public G2PAppBase
 {
 public:
-    G2PMaterial(const char *name, double z, double a, double x0, double density);
+    G2PMaterial(const char *name); // Read parameters from configuration file
+    G2PMaterial(const char *name, double z, double a, double x0, double density, double ion, double c);
     virtual ~G2PMaterial();
+
+    virtual int Begin();
 
     virtual double EnergyLoss(double E, double l);
     virtual double MultiScattering(double E, double l);
+    virtual double InterBremsstrahlung(double E, double angle);
+
+    // Sets
+
+    // Gets
+    double GetRadLen();
+    double GetDensity();
 
 protected:
     G2PMaterial(); // Only for ROOT I/O
 
     double Ionization(double E, double l);
     double FluctIonization(double E, double meanloss);
-    double Bremsstrahlung(double E, double l);
-
+    double ExterBremsstrahlung(double E, double l);
     virtual int Configure(EMode mode = kTWOWAY);
     virtual void MakePrefix();
 
     const char *fName;
+
     int fZ, fA;
     double fMass;
     double fDensity; // density in g/cm^3
+
     double fX0; // radiation length in g/cm^2
+
     double fB; // value of b_func()
+    double fIon;  //Ionizatin energy, eV
+    double fC;  //density correction factor
 
 private:
-    static G2PAppList *pG2PMaterial;
-
     ClassDef(G2PMaterial, 1)
 };
 
--- src/G2PMaterial.cc
+++ src/G2PMaterial.cc
@@ -8,7 +8,8 @@
 
 // History:
 //   Sep 2013, C. Gu, First public version.
-//   Oct 2013, J. Liu, Check formulas.
+//   Oct 2013, J. Liu, Check formulas
+//   Nov 2014, J. Liu, Add ionization fluctuation, internal and external bremsstrahlung.
 //
 
 #include <cstdlib>
@@ -28,10 +29,7 @@
 
 using namespace std;
 
-static const double kELECTRONMASS = 0.510998918; // MeV
-
-// Ionization potentials - source : NIST
-static const double kIONI[] = {19.2, 41.8, 40.0, 63.7, 76.0, 78.0, 82.0, 95.0, 115.0, 137.0, 149.0, 156.0, 166.0, 173.0, 173.0, 180.0, 174.0, 188.0, 190.0, 191.0, 216.0, 233.0, 245.0, 257.0, 272.0, 286.0, 297.0, 311.0, 322.0, 330.0, 334.0, 350.0, 347.0, 348.0, 343.0, 352.0, 363.0, 366.0, 379.0, 393.0, 417.0, 424.0, 428.0, 441.0, 449.0, 470.0, 470.0, 469.0, 488.0, 488.0, 487.0, 485.0, 491.0, 482.0, 488.0, 491.0, 501.0, 523.0, 535.0, 546.0, 560.0, 574.0, 580.0, 591.0, 614.0, 628.0, 650.0, 658.0, 674.0, 684.0, 694.0, 705.0, 718.0, 727.0, 736.0, 746.0, 757.0, 790.0, 790.0, 800.0, 810.0, 823.0, 823.0, 830.0, 825.0, 794.0, 827.0, 826.0, 841.0, 847.0, 878.0, 890.0};
+static const double kMe = 0.510998918; // MeV
 
 static double b_func(int Z)
 {
@@ -68,87 +66,113 @@ static double b_func(int Z)
     return (4.0 / 3.0) * (1.0 + (1.0 / 9.0) * (Z + 1) / (Lrad * Z + Lradp));
 }
 
-G2PAppList *G2PMaterial::pG2PMaterial = new G2PAppList();
-
-G2PMaterial::G2PMaterial() : fName(NULL)
+G2PMaterial::G2PMaterial()
 {
     // Only for ROOT I/O
 }
 
-G2PMaterial::G2PMaterial(const char *name, double z, double a, double x0, double density) :
-    fName(name), fZ(z), fA(a), fMass(0), fDensity(density), fX0(x0)
+G2PMaterial::G2PMaterial(const char *name) : fName(name)
 {
-    // Constructor
+    // Nothing to do
+}
+
+G2PMaterial::G2PMaterial(const char *name, double z, double a, double x0, double density, double ion, double c) : fName(name), fZ(z), fA(a), fMass(0), fDensity(density), fX0(x0), fIon(ion), fC(c)
+{
+    // If use this constructor, no need to call the Begin() function
 
     fB = b_func(fZ);
-
-    pG2PMaterial->Add(this);
 }
 
 G2PMaterial::~G2PMaterial()
 {
-    // Destructor
+    // Nothing to do
+}
 
-    pG2PMaterial->Remove(this);
+int G2PMaterial::Begin()
+{
+    if (G2PAppBase::Begin() != 0)
+        return (fStatus = kBEGINERROR);
+
+    fB = b_func(fZ);
+
+    return (fStatus = kOK);
 }
 
 double G2PMaterial::EnergyLoss(double E, double l)
 {
     double EMeV = E * 1000; // MeV
     double lcm = l * 100; // cm
-    assert(l > -1.0e-8);
+
+    assert(lcm >= 0);
 
     double result = 0;
     result += Ionization(EMeV, lcm);
-    result += Bremsstrahlung(EMeV, lcm);
+    result += ExterBremsstrahlung(EMeV, lcm);
 
-    return result / 1000.0; // GeV
+    if (result > (E - kMe))
+        result = E - kMe;
+
+    if (result < 0)
+        result = 0;
+
+    if (result > 0)
+        return result / 1000.0; // GeV
+
+    return 0;
 }
 
 double G2PMaterial::MultiScattering(double E, double l)
 {
-    // only for electron
+    // Only for electron
 
-    double EMeV = E * 1000;
-
+    double EMeV = E * 1000; // MeV
     double thicknessr = (l * 100) * fDensity / fX0; // l: m -> cm
-    assert(l > -1.0e-8);
 
-    double lPsq = EMeV * EMeV - kELECTRONMASS * kELECTRONMASS;
+    assert(l >= 0);
+
+    double lPsq = EMeV * EMeV - kMe * kMe;
     double bcp = lPsq / EMeV;
     double ltheta0 = 13.6 / bcp * sqrt(thicknessr) * (1 + 0.038 * log(thicknessr));
 
-    if (thicknessr != 0) {
-        return pRand->Gaus(0, ltheta0);    // rad
-    } else
-        return 0.0;
+    if (thicknessr != 0)
+        return pRand->Gaus(0, ltheta0); // rad
+
+    return 0.0;
+}
+
+double G2PMaterial::GetRadLen()
+{
+    return fX0;
+}
+
+double G2PMaterial::GetDensity()
+{
+    return fDensity;
 }
 
 double G2PMaterial::Ionization(double E, double l)
 {
     // Calculate ionization dE/dx by Seltzer-Berger formula
 
-    double fcut = 5;
+    double fcut = 6;
     double thickness = l * fDensity;
     double lK = 0.307075; // cm^2/g for A=1 g/mol
-    double tau = E / kELECTRONMASS - 1;
+    double tau = E / kMe - 1;
     double gamma = tau + 1.0;
     double gamma2 = gamma * gamma;
     double bg2 = tau * (tau + 2);
     double beta2 = bg2 / gamma2;
 
-    double eexc = kIONI[fZ] * 1e-6 / kELECTRONMASS;
-    double I = kIONI[fZ] * 1e-6;
+    double eexc = fIon * 1e-6 / kMe;
+    // double I = kIONI[fZ]* 1e-6;
     double eexc2 = eexc * eexc;
-    double d = fcut / kELECTRONMASS;
+    double d = fcut / kMe;
     double dedx = log(2.0 * (tau + 2.0) / eexc2) - 1.0 - beta2 + log((tau - d) * d) + tau / (tau - d) + (0.5 * d * d + (2.0 * tau + 1.) * log(1. - d / tau)) / gamma2;
 
-    double hnup = 28.816 * sqrt(fDensity * fZ / fA) * 1e-6; // MeV // fDensity is density of absorber
-
-    //density correction
+    // density correction
     double x = 0.5 * log10(bg2);
-    double C = 2.0 * log(I / hnup) + 1.0;
-    double delta = 4.6052 * x - C;
+
+    double delta = 4.6052 * x - fC;
     dedx -= delta;
 
     // now you can compute the total ionization loss
@@ -159,8 +183,8 @@ double G2PMaterial::Ionization(double E, double l)
 
     double result = FluctIonization(E, dedx);
 
-    if (result > (E - kELECTRONMASS))
-        result = E - kELECTRONMASS;
+    if (result > (E - kMe))
+        result = E - kMe;
 
     if (result < 0)
         result = 0;
@@ -173,8 +197,8 @@ double G2PMaterial::FluctIonization(double E, double meanloss)
     // Calculate actual ionization energy loss from mean loss
     // Urban model
 
-    double fcut = 5;
-    double tau = E / kELECTRONMASS - 1;
+    double fcut = 6;
+    double tau = E / kMe - 1;
     double gamma = tau + 1.0;
     double gamma2 = gamma * gamma;
     double bg2 = tau * (tau + 2);
@@ -196,8 +220,8 @@ double G2PMaterial::FluctIonization(double E, double meanloss)
     double f1fluct = 1. - f2fluct;
     double e2fluct = 10. * fZ * fZ * eV;
     double loge2fluct = log(e2fluct);
-    double ipotfluct = kIONI[fZ] * eV;
-    double logipotfluct = log(kIONI[fZ] * eV);
+    double ipotfluct = fIon * eV;
+    double logipotfluct = log(fIon * eV);
     double loge1fluct = (logipotfluct - f2fluct * loge2fluct) / f1fluct;
     double e1fluct = exp(loge1fluct);
     double e0 = 10. * eV;
@@ -225,7 +249,7 @@ double G2PMaterial::FluctIonization(double E, double meanloss)
         double a1 = 0., a2 = 0., a3 = 0.;
 
         if (tmax > ipotfluct) {
-            double w2 = log(2. * kELECTRONMASS * beta2 * gamma2) - beta2;
+            double w2 = log(2. * kMe * beta2 * gamma2) - beta2;
 
             if (w2 > logipotfluct) {
                 double C = meanloss * (1. - rate) / (w2 - logipotfluct);
@@ -338,38 +362,102 @@ double G2PMaterial::FluctIonization(double E, double meanloss)
     return losstot;
 }
 
-double G2PMaterial::Bremsstrahlung(double E, double l)
+double G2PMaterial::InterBremsstrahlung(double E, double angle)
 {
-    // Bremsstrahlung Energy Loss for external and internal(equivalent radiator)
-    // Xiaodong Jiang, PhD. thesis Equ (5.15)
-    // http://filburt.mit.edu/oops/Html/Pub/theses/xjiang.ps
-    // *0.999 to avoid lose all energy
+    // Take from gener_cone MC, modified by R.M
+    // Modified by Jie Liu, Nov 25 2014
 
-    double bt = l * fDensity / fX0 * fB;
+    // L.Van Hoorebeke, University of Gent, e-mail: Luc.VanHoorebeke@UGent.be
+    // This function generates internal radiation the distribution used
+    // contains multiple emission effects
+    // k: electron momentum (GeV)
+    // nu: equivalent radiator length in units radiation length
+    //
+    //     this is actually half the equivalent radiator length
+    //     because 1/2 placed before and 1/2 placed after proton
 
-    double result = 0;
+    double qsq = 2 * E * E * (1 - cos(angle));
+    double alpha = (1. / 137.);
+    double bval = 4. / 3.;
+    double msq = 2.6112e-7; // mass electron squared (GeV^2)
+    // This is the equivalent radiator used for internal bremsstrahlung.
+    double nu = (alpha / (bval * 3.141592627)) * (log(qsq / msq) - 1);
+    double  cut, Ekin, prob, prob_sample, sample;
 
-    if (bt != 0)
-        result = E * pow(pRand->Uniform() * 0.999, 1. / bt);
+    // Initialization of lower limit of bremsstrahlung (1 keV)
+    cut = 1e-6;
 
-    if (result > (E - kELECTRONMASS))   // MeV
-        result = E - kELECTRONMASS;
+    Ekin = E - kMe;
 
-    if (result < 0)
-        result = 0;
+    // Calculation of probability to have internal radiation effect above 1 keV. *
+    prob = 1. - pow(cut / Ekin, nu);
 
-    return result; // MeV
+    prob_sample = pRand->Uniform();  // Random sampling
+
+    if (prob_sample > prob)
+        return 0.;
+
+    // bremsstrahlung has taken place! Generate photon energy
+    sample = pRand->Uniform();
+
+    return Ekin * pow(sample * prob + pow(cut / Ekin, nu), 1. / nu);
+}
+
+double G2PMaterial::ExterBremsstrahlung(double E, double l)
+{
+    //  taken from gener_cone MC, modified by R.M
+    //  modified by Jie Liu, Nov 25 2014
+
+    // L.Van Hoorebeke, University of Gent, e-mail: Luc.VanHoorebeke@UGent.be, luc@inwfsun1.rug.ac.be
+    // This function generates external bremsstrahlung in the target
+    // the distribution used contains multiple emission effects
+    // k: electron momentum
+    // fracrl = fraction of radiation lengths distance, traveled through material.
+
+    double cut, Ekin, bt, prob, prob_sample;
+    double sample, xtry, env, value, ref;
+    double Euler = 0.5772157;
+
+    // Initialization of lower limit of bremsstrahlung (1 keV)
+    cut = 0.001;
+    Ekin = E - kMe;
+
+    bt = l * fDensity / fX0 * fB;
+
+    // Calculation of probability to have bremsstrahlung effect above 1 keV
+    prob = 1. - pow(cut / Ekin, bt) - bt / (bt + 1.) * (1. - pow(cut / Ekin, bt + 1.))
+           + 0.75 * bt / (2. + bt) * (1. - pow(cut / Ekin, bt + 2.));
+    prob = prob / (1. - bt * Euler + bt * bt / 2.*(Euler * Euler + 3.1415926 * 3.1415926 / 6.)); /* Gamma function */
+
+    prob_sample = pRand->Uniform();
+
+    if (prob_sample > prob)
+        return 0.;
+
+    // Bremsstrahlung has taken place! Generate photon energy with sample and reject
+    // using 1/x as envelope
+
+    do {
+        sample = pRand->Uniform();
+        xtry = cut * pow(Ekin / cut, sample);
+
+        env = 1. / xtry;
+        value = 1. / xtry * (1. - xtry / Ekin + 0.75 * pow(xtry / Ekin, 2)) * pow(xtry / Ekin, bt);
+
+        sample = pRand->Uniform();
+        ref = value / env;
+    } while (sample > ref);
+
+    return xtry;
 }
 
 int G2PMaterial::Configure(EMode mode)
 {
-    if (mode == kREAD || mode == kTWOWAY) {
-        if (fIsInit)
-            return 0;
+    if ((mode == kREAD || mode == kTWOWAY) && fConfigured)
+        return 0;
 
-        else
-            fIsInit = true;
-    }
+    if (G2PAppBase::Configure(mode) != 0)
+        return -1;
 
     ConfDef confs[] = {
         {"z", "Z", kINT, &fZ},
@@ -377,6 +465,8 @@ int G2PMaterial::Configure(EMode mode)
         {"mass", "Mass", kDOUBLE, &fMass},
         {"density", "Density", kDOUBLE, &fDensity},
         {"radlen", "Radiation Length", kDOUBLE, &fX0},
+        {"ionization", "ionizationexec", kDOUBLE, &fIon},
+        {"correction", "density correction", kDOUBLE, &fC},
         {0}
     };
 
